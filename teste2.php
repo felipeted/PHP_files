0f9cadd63358d401679ecb6daa0273a4c4846043
felipeted <40530168+felipeted@users.noreply.github.com>
2018-07-31 16:03:21 -0300

    Update teste2.php

--- teste2.php
+++ teste2.php
@@ -1,15 +1,16 @@
 <?php
-echo "<p>programa que pega as terças feiras treze de 1970 até o ano 2020</p>";
 
 setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
 
 $ts = strtotime("1970-01-01");
-$ta = $ts+86400;
+$ta = strtotime("now");
 $final = strtotime("+2 Years");
 
-echo date("l d/m/Y", $ts)."<br>";
-echo date("l d/m/Y", $ta)."<br>";
-echo date("l d/m/Y", $final)."<br>";
+echo "<p>programa que pega as terças feiras treze de 1970 até o ano ".date("Y" , $final)."</p>";
+
+echo "Início: ".date("l d/m/Y", $ts)."<br>";
+echo "Hoje: ".date("l d/m/Y", $ta)."<br>";
+echo "Fim: ".date("l d/m/Y", $final)."<br><br>";
 
 while (date("Y", $ts)<2020) {
 	while ($ts<$final) {
